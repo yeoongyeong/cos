102bf00630d4fdfcad96bd2876f995481bb066a5
yeoongyeong <yeongyeongjo05@gmail.com>
2023-04-25 15:56:16 +0900

    최종수정

--- js/core.js
+++ js/core.js
@@ -12,23 +12,22 @@ $(document).ready(function(){
     autoHeight(".serviceMenu",".serviceMenu aside");
     fitvidsBox("#map");
     toggleAction(".mui");
-    // toggleButton("footer div:nth-of-type(2) h3");
 });
 function fitvidsBox(target){
     $(target).fitVids();
 }
 function rollingBanner(){
-    var banTarget = $(".rollingBanner li"); // li들을 banTarget에 담기. (2개) 
-    var i = 0; // n번째 li를 지정하기 위해 i 를 0으로 초기화 (0번 li부터 시작)
+    var banTarget = $(".rollingBanner li"); 
+    var i = 0; 
     setInterval(function(){
-        var currentBanner = banTarget.eq(i); // eq = n번째 대상구하기. eq(0) => 첫번째 li.
-        banTarget.removeClass("active"); // 일단 전체 li active 삭제
-        currentBanner.addClass("active"); // 현재 eq 번호에 해당하는 li에게 active 부여.
-        i++; // i를 1증가
-        if(i == banTarget.length){ // li의 갯수 구해와서 (현재: 2) i와 같을 때 i를 0으로 돌려놓으라.
+        var currentBanner = banTarget.eq(i); 
+        banTarget.removeClass("active");
+        currentBanner.addClass("active");
+        i++;
+        if(i == banTarget.length){ 
             i = 0;
         }
-    }, 2000); // 지정된시간(2000 m/s = 2초)마다 앞에 담긴 애를 실행.
+    }, 2000); 
 }
 function slider(target,pagerValue,maxVal,minVal,slideW){
     $(target).bxSlider({
@@ -72,20 +71,16 @@ function guidePanelInner(openBtn){
     });
 }
 function detailScroll(target){
-    var scrollLock = $(".detailContainer ul").height() - $(target).height(); // 우측면 aside의 높이 구하기.
+    var scrollLock = $(".detailContainer ul").height() - $(target).height(); 
 
-    $(window).scroll(function(){ // browser에 scroll이 작동 되었을 때
-        if($(this).scrollTop() >= scrollLock){ // 만약 browser의 scrollTop(스크롤막대의윗면)이 aside 높이의 절반보다 커지면
-            $(target).addClass("lock"); // aside에 lock 걸기
-        }else{ // 그 외 나머지 상황엔
-            $(target).removeClass("lock"); // aside에 lock 풀기
+    $(window).scroll(function(){                    
+        if($(this).scrollTop() >= scrollLock){ 
+            $(target).addClass("lock"); 
+        }else{ 
+            $(target).removeClass("lock");  
         }
     });
 }
-// function precautions(target){
-
-// }
-
 function autoHeight(target,follow){
     if (window.matchMedia("(min-width: 1280px)").matches) {
         var targetHeight = $(target).height();
@@ -97,9 +92,3 @@ function toggleAction(target){
         $(this).toggleClass("active");
     })
 }
-
-// function toggleButton(target){
-//     $(target).click(function(){
-//         $(this).toggleClass("active");
-//     });
-// }
